8fcd82915cd248d9622ab56b8eaff750265899d3
tonypsc <tonyps@gmail.com>
2023-05-30 17:17:35 -0400

    Add comments to AsteroidFetchRepository

--- src/contexts/asteroid/infrastructure/persistance/AsteroidsFetchRepository.ts
+++ src/contexts/asteroid/infrastructure/persistance/AsteroidsFetchRepository.ts
@@ -17,11 +17,18 @@ class AsteroidFetchRepository
 		this.apiKey = apiKey;
 	}
 
+	/**
+	 * Calls the api to get the list of asteroids
+	 * @param initialDate
+	 * @param endDate
+	 * @returns
+	 */
 	async getByDate(initialDate: string, endDate: string) {
 		const result = (await this.get(
 			`${this.baseUrl}?start_date=${initialDate}&end_date=${endDate}&api_key=${this.apiKey}`
 		)) as AsteroidApiResult;
 
+		// create a plain array of asteroids, removing date key
 		const asteroids = Object.keys(result.near_earth_objects).reduce(
 			(asteroids, date) => {
 				const dateAsteroids = result.near_earth_objects[date];
@@ -33,6 +40,11 @@ class AsteroidFetchRepository
 		return asteroids;
 	}
 
+	/**
+	 * Calls the api to get the detail of an asteroid
+	 * @param id
+	 * @returns
+	 */
 	async getById(id: string) {
 		const result = await this.get(
 			`${this.baseUrl}/${id}?api_key=${this.apiKey}`
